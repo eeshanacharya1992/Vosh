ca9c806c7b4644d9b67abc71aaf30dfd356a8b11
Eeshan <eeshanacharya@gmail.com>
2024-03-09 00:49:53 +0530

    Java

---
+++ JavaProject/src/Package/HJi.java
@@ -0,0 +1,10 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class HJi {
+        @Test
+        public void add() {
+        	System.out.println("hi");
+        }
+}
---
+++ JavaProject/src/Package/Hji2.java
@@ -0,0 +1,10 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class Hji2 {
+	 @Test
+     public void add() {
+     	System.out.println("hi33");
+     }
+}
